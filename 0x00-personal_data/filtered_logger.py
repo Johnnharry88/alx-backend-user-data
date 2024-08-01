4caff6b015e660aa7e2beb42acaa50f95e60d93a
Johnharry88 <harryjohnwhye@gmail.com>
2024-08-01 12:41:43 +0100

    PData

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""Module that selects items using reges, porecess using log
+fomatter, creates a logger, connects to a secure database,
+reads and filters data"""
+import re
+from typing import List
+import logging
+from os import getenv
+import mysql.connector
+
+
+# PII FIELDS contains fields of user that considered personal data
+PII_FIELDS = ('name', 'password', 'phone', 'ssn', 'email')
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, seperator: str) -> str:
+    """ Returns Obfscated log message"""
+    for f in fields:
+        message = re.sub(f+'=.*?'+seperator,
+                         f+'='+redaction+seperator, message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filters values from records moving into log reords
+        using filter_datum"""
+        ms = super(RedactingFormatter, self).format(record)
+        output = filter_datum(self.fields, self.REDACTION, ms, self.SEPARATOR)
+        return output
+
+
+def get_logger() -> logging.Logger:
+    """Functions that returns an object of logging.Logger"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    streamer = logging.StreamHandler()
+    streamer.setFormatter(RedactingFormatter(PII_FILES))
+    logger.addHandler(streamer)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Connects to our database"""
+    user = getenv('PERSONAL_DATA_DB_USERNAME') or 'root'
+    pasdkey = getenv('PERSONAL_DATA_DB_PASSWORD') or ""
+    host = getenv('PERSONAL_DATA_DB_HOST')
+    db = getenv('PERSONAL_DATA_DB_NAME')
+    connect = mysql.connector.connect(
+        user=user,
+        password=pasdkey,
+        host=host,
+        database=db,
+        )
+    return connect
