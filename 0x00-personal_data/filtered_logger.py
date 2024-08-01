a39b22b23fa3f877f0c2a2efd759f8e30260d8a0
Johnharry88 <harryjohnwhye@gmail.com>
2024-08-01 14:32:01 +0100

    PData

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -55,9 +55,9 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """Connects to our database"""
-    user = os.getenv('PERSONAL_DATA_DB_USERNAME')
-    pasdkey = os.getenv('PERSONAL_DATA_DB_PASSWORD')
-    host = os.getenv('PERSONAL_DATA_DB_HOST')
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME') or 'root'
+    pasdkey = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ''
+    host = os.getenv('PERSONAL_DATA_DB_HOST') or 'localhost'
     db = os.getenv('PERSONAL_DATA_DB_NAME')
     connect = mysql.connector.connect(
         user=user,
