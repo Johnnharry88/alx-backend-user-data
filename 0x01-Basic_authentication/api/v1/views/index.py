93d77bb8eda00c2d7a7d38692fd71791dc82cc0e
Johnharry88 <harryjohnwhye@gmail.com>
2024-08-05 21:26:17 +0100

    Auth

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def authorized() -> str:
+    """GGET api unauthorised"""
+    abort(401, description="Unauthorized")
